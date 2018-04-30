af427c992225ac1a4872a5806a3aa73f8eccbea8
David Ceron <dacer_78@hotmail.com>
2018-04-29 23:58:23 -0500

    Se actualiza la base de datos

--- schema.sql
+++ schema.sql
@@ -7,11 +7,7 @@ USE indentation_war;
 CREATE TABLE team(
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
+	twitter VARCHAR(50) NOT NULL,
+	country VARCHAR(20) NOT NULL,
 	side VARCHAR(10) NOT NULL
 );
-
-
-/*
-INSERT INTO team (campo1, campo2) VALUES (valor1, valor2);
-INSERT INTO team SET campo1 = valor1, campo2 = valor2;
-*/
